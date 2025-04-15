47c9716daa8cd7f7d3f9b907a87f425ad6e1b0bb
dftortosa <dsalazar@ugr.es>
2025-04-15 13:00:54 +0200

    starting the script for the summarization of PRS results

--- association_studies/scripts/production_scripts/03d_processing_results.py
+++ association_studies/scripts/production_scripts/03d_processing_results.py
@@ -1,5 +1,165 @@
+#!/usr/bin/env python3.9
+# coding: utf-8
+    #to run this script: chmod +x script.py; ./script.py
+    #!/bin/sh does not work with my terminal en msi of David.
+    #if you are using "$" to paste the path of the executable, you do not need to use "./" for running the executable.
+    #you can save the output and the errors
+        #./script.py > script.out #only output
+        #./script.py 2> error.out #only error
+        #./script.py > script.out 2> error.out #both in different files
+        #./script.py > script.out 2>&1 #both in the same file
+        #https://www.cyberciti.biz/faq/linux-redirect-error-output-to-file/
 
-###process prredictive power and make manhatann plots
+
+
+#############################################
+######## PROCESS ASSOCIATION RESULTS ########
+#############################################
+
+#This script will process the results of the association studies, summarizing the predictive power of the PRS across CV iterations and generating Manhattan plots.
+
+#This and previous scripts are based on the following tutorials:
+    #1. Quality Control Procedures for Genome-Wide Association Studies
+        #https://github.com/RitchieLab/GWAS-QC
+        #https://drive.google.com/file/d/1kxV3j_qCF_XMX47575frXhVwRMhzjqju/view
+    #2. Genome-wide association studies
+        #https://www.nature.com/articles/s43586-021-00056-9
+    #3. Data Management and Summary Statistics with PLINK
+        #https://link.springer.com/protocol/10.1007/978-1-0716-0199-0_3#Sec22
+    #4. Genomics Boot Camp
+        #https://genomicsbootcamp.github.io/book/
+    #5. Tutorial: a guide to performing polygenic risk score analyses
+        #https://www.nature.com/articles/s41596-020-0353-1
+        #https://choishingwan.github.io/PRS-Tutorial/
+    #6. Genome wide association study of response to interval and continuous exercise training: the Predict-HIIT study
+        #https://jbiomedsci.biomedcentral.com/articles/10.1186/s12929-021-00733-7
+    #7. Omics Data Preprocessing for Machine Learning: A Case Study in Childhood Obesity
+        #https://www.mdpi.com/2073-4425/14/2/248
+    #8. LDAK
+        #https://dougspeed.com/
+        #https://www.nature.com/articles/s41467-021-24485-y
+
+
+
+#######################################
+# region INITIAL ANOTATIONS AND STEPS #
+#######################################
+
+
+###########
+# imports #
+###########
+
+import pandas as pd
+import numpy as np
+
+
+########################################
+# define function to print text nicely #
+########################################
+
+#text="checking function to print nicely"
+#header=1
+def print_text(text, header=2):
+    if header==1:
+        print("\n#######################################\n#######################################")
+        print(text)
+        print("#######################################\n#######################################")
+    elif header==2:
+        print("\n###### " + text + " ######")
+    elif header==3:
+        print("\n## " + text + " ##")
+    elif header==4:
+        print("\n# " + text + " #")
+print_text("checking function to print nicely: header 1", header=1)
+print_text("checking function to print nicely: header 2", header=2)
+print_text("checking function to print nicely: header 3", header=3)
+print_text("checking function to print nicely: header 4", header=4)
+
+
+
+########################################
+# define function to run bash commands #
+########################################
+
+#create a wrapper for subprocess.run in order to define a set of arguments and avoid typing them each time. We will ensure that we are using bash always and not sh.
+from subprocess import run, PIPE
+#command="ls"
+def run_bash(command, return_value=False):
+
+    #run the command
+    complete_process = run(
+        command, 
+        shell=True,
+        executable="/bin/bash", 
+        stdout=PIPE,
+        stderr=PIPE, 
+        text=True)
+    #we have to use popen in order to ensure we use bash, os.system does not allow that
+        #shell=True to execute the command through the shell. This means that the command is passed as a string, and shell-specific features, such as wildcard expansion and variable substitution, can be used.
+            #THIS IS DANGEROUS IF UNTRUSTED DATA
+        #executable="/bin/bash" to ensure the use of bash instead of sh
+        #stdout=PIPE to capture the output into an python object. You can also capture the error doing stderr=PIPE. stdout and stderr are the standard output and error
+            #you could also use capture_output=True to capture both stdout and stderr
+        #text=True will return the stdout and stderr as string, otherwise as bytes
+            #https://www.datacamp.com/tutorial/python-subprocess
+            #https://docs.python.org/3/library/subprocess.html#subprocess.run
+
+    #this generated a CompletedProcess instance where you can get
+        #args: The command and arguments that were run.
+        #returncode: The return code of the subprocess.
+        #stdout: The standard output of the subprocess, as a bytes object.
+        #stderr: The standard error of the subprocess, as a bytes object.
+
+    #if the return code is 0, i.e., success 
+        #https://askubuntu.com/a/892605
+    if complete_process.returncode==0:
+
+        #if stderr is empty
+        if complete_process.stderr=="":
+
+            #print the standard output without "\n" and other characters
+            print(complete_process.stdout)
+
+            #return also the value if required
+            if return_value==True:
+                return complete_process.stdout
+        else:
+
+            #print the standard output without "\n" and other characters
+            print(complete_process.stdout)
+
+            #print the standard error without stopping
+            print(complete_process.stderr)
+
+            #return also the value if required
+            if return_value==True:
+                return complete_process.stdout
+    else:
+        #print the standard error and stop
+        raise ValueError("ERROR: FALSE! WE HAVE A PROBLEM RUNNING COMMAND: " + complete_process.stderr)
+
+#test it
+print_text("check behaviour run_bash", header=1)
+print_text("see working directory", header=2)
+run_bash("pwd")
+print_text("list files/folders there", header=2)
+run_bash("ls")
+
+# endregion
+
+
+
+
+
+
+###########################
+# region Summary strategy #
+###########################
+
+
+
+# endregion
 
 
 #in the results of --linear
